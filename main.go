ec3bcc8a50f481e31d1e359df64e06c3d941e7ba
Jean Patrick <jean_patrick115@hotmail.com>
2024-12-09 21:22:54 -0300

    feat: add schemas and custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/jeanpatrickm/job-manager/router"
+import (
+	"github.com/jeanpatrickm/job-manager/config"
+	"github.com/jeanpatrickm/job-manager/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("[MAIN]")
+	// initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
+
+	// initialize Router
 	router.InitializeRouter()
 }
